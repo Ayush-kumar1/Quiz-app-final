f5b1bbeccf0cfc0f10ed6c16c2001ab5f7b8ca6c
Ayush <ayush.ak5555@gmail.com>
2021-05-23 15:59:47 +0530

    Files added from local branch

---
+++ src/API.ts
@@ -0,0 +1,47 @@
+import { useCategory } from './CategoryContext';
+import { shuffleArray } from './utils';
+
+
+
+
+
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export type QuestionsState = Question & { answers: string[] };
+
+
+  // const{category}=useCategory()
+
+ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+
+ ;
+  // const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const endpoint='https://opentdb.com/api.php?amount=10&category=21&type=multiple'//sports
+  // const endpoint="https://opentdb.com/api.php?amount=10&category=15&type=multiple"//video-game
+  // const endpoint="https://opentdb.com/api.php?amount=10&category=15&type=multiple"//anime
+  const data = await (await fetch(endpoint)).json();
+
+  console.log(data)
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
+};
+
+
+
+
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { CategoryProvider } from './CategoryContext';
+import Home from "./Home"
+import { BrowserRouter as Router } from "react-router-dom"
+import Mediator from "./Mediator"
+
+ReactDOM.render(
+  <React.StrictMode>
+    
+    {/* <App /> */}
+   <Mediator/>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
